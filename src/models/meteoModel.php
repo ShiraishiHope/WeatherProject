1c2fc57d3c3682d53666f6bb3bfcfced05923724
vmichel4810 <v.michel@it-students.fr>
2023-01-13 17:02:38 +0100

    weathercodeImg.php added in model folder

--- src/models/meteoModel.php
+++ src/models/meteoModel.php
@@ -1,31 +1,37 @@
 <?php
 class Meteo {
+    public $latitude;
+    public $longitude;
+    public $data;
+
+
     public function __construct($latitude, $longitude) {
         $this->latitude = $latitude;
         $this->longitude = $longitude;
         $json_data = file_get_contents("https://api.open-meteo.com/v1/dwd-icon?latitude=" . $this->latitude . "&longitude=" . $this->longitude . "&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=auto&start_date=2023-01-12&end_date=2023-01-15");
         $this->data = json_decode($json_data);
     }
-    getHourlyTemp(){
+    function getHourlyTemp(){
         return $this->data->hourly->temperature_2m;
     }
-    getHourlyHumidity(){
+    function getHourlyHumidity(){
         return $this->data->hourly->relativehumidity_2m;
     }
-    getHourlyWeathercode(){
+    function getHourlyWeathercode(){
         return $this->data->hourly->weathercode;
     }
-    getDailyMinTemp(){
+    function getDailyMinTemp(){
         return $this->data->daily->temperature_2m_min;
     }
-    getDailyMaxTemp(){
+    function getDailyMaxTemp(){
         return $this->data->daily->temperature_2m_max;
     }
-    getDailyWeathercode(){
+    function getDailyWeathercode(){
         return $this->data->daily->weathercode;
     }
-    getDailyWindSpeed(){
+    function getDailyWindSpeed(){
         return $this->data->daily->windspeed_10m_max;
     }
+
 }
 ?>
