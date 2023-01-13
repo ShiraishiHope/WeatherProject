cab9ad3db55ae67eb5311783e83422b611e5ef7b
Ryan Mazri <ryan.mazri@yahoo.fr>
2023-01-13 15:57:04 +0100

    v0.2.0: finished class Meteo which gets the information from the Open Meteo API and returns arrays of value with getters.

--- src/models/meteoModel.php
+++ src/models/meteoModel.php
@@ -1,11 +1,31 @@
+<?php
 class Meteo {
-    constructor(latitude, longitude) {
-        this->latitude = latitude;
-        this->longitude = longitude;
-        const json_data = file_get_contents("https://api.open-meteo.com/v1/dwd-icon?latitude=" + this.latitude + "&longitude=" + this.longitude + "&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=auto&start_date=2023-01-12&end_date=2023-01-15");
-        this->data = json_decode($json_data);
+    public function __construct($latitude, $longitude) {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        $json_data = file_get_contents("https://api.open-meteo.com/v1/dwd-icon?latitude=" . $this->latitude . "&longitude=" . $this->longitude . "&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=auto&start_date=2023-01-12&end_date=2023-01-15");
+        $this->data = json_decode($json_data);
     }
-    getTemperature(){
-        return this->data->daily->[$i];
+    getHourlyTemp(){
+        return $this->data->hourly->temperature_2m;
+    }
+    getHourlyHumidity(){
+        return $this->data->hourly->relativehumidity_2m;
+    }
+    getHourlyWeathercode(){
+        return $this->data->hourly->weathercode;
+    }
+    getDailyMinTemp(){
+        return $this->data->daily->temperature_2m_min;
+    }
+    getDailyMaxTemp(){
+        return $this->data->daily->temperature_2m_max;
+    }
+    getDailyWeathercode(){
+        return $this->data->daily->weathercode;
+    }
+    getDailyWindSpeed(){
+        return $this->data->daily->windspeed_10m_max;
     }
 }
+?>
