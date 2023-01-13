c26c4145b04bcda68b40502b514c4cdba486ddd4
wikkly <sophiemajorel@hotmail.fr>
2023-01-13 15:10:17 +0100

    v.0.1.0 add homepage, display weather by five

---
+++ views/home/home.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>WeatherApp</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+    <!-- <?php include_once "weather3days.php" ?>  -->
+</head>
+<body>
+<section>
+     <p><label for="city">City :</label><p>
+    <input type="text" id="city" name="city" required>
+    <button type="submit"> Choisir ma ville </button>
+    <p><?php echo date('l jS \of F Y h:i A'); ?></p>
+    <span> <img class=imgnow src="https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png" alt="icone"> </span>
+    <p> 26°C <p>
+</section>
+    <div class="section1">
+        <div>
+            <p> Now </p>
+            <img src="https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png" alt="icone"> <br>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/850/850785.png" alt="icone">
+            <p> 14% </p>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/172/172922.png" alt="icone">
+            <p> 4 m/s <p>
+        </div>
+        <div>
+            <p> Now </p>
+            <img src="https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png" alt="icone"> <br>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/850/850785.png" alt="icone">
+            <p> 14% </p>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/172/172922.png" alt="icone">
+            <p> 4 m/s <p>
+        </div>
+        <div>
+            <p> Now </p>
+            <img src="https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png" alt="icone"> <br>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/850/850785.png" alt="icone">
+            <p> 14% </p>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/172/172922.png" alt="icone">
+            <p> 4 m/s <p>
+        </div>
+        <div>
+            <p> Now </p>
+            <img src="https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png" alt="icone"> <br>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/850/850785.png" alt="icone">
+            <p> 14% </p>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/172/172922.png" alt="icone">
+            <p> 4 m/s <p>
+        </div>
+        <div>
+            <p> Now </p>
+            <img src="https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png" alt="icone"> <br>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/850/850785.png" alt="icone">
+            <p> 14% </p>
+            <img class="imgsmaller" src="https://cdn-icons-png.flaticon.com/512/172/172922.png" alt="icone">
+            <p> 4 m/s <p>
+        </div>
+    </div>
+</body>
+</html>
