946d27831144a749f12b6cb3d0ab7176cf4ef9b4
“AxLDvL” <a.denis@it-students.fr>
2023-01-16 22:47:57 +0100

    v0.3.6 fixed home.php img href and meteoModel

--- src/models/meteoModel.php
+++ src/models/meteoModel.php
@@ -17,7 +17,7 @@ class Meteo {
     function requestAPI(object $class){
         $today = date('Y-m-d');
         $lastDay = date('Y-m-d',strtotime($today.'+6 days'));
-        $openMeteoJson = file_get_contents("https://api.open-meteo.com/v1/dwd-icon?latitude=" . $class->getLatitude() . "&longitude=" . $class->getLongitude() . "&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=auto&start_date=".$today."&end_date=".$lastDay);
+        $openMeteoJson = file_get_contents("https://api.open-meteo.com/v1/dwd-icon?latitude=" . $class->getLatitude() . "&longitude=" . $class->getLongitude() . "&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max&timezone=auto&start_date=".$today."&end_date=".$today);
         $openMeteoObject = json_decode($openMeteoJson);
         echo $class->getLatitude();
         echo $today;
@@ -25,13 +25,13 @@ class Meteo {
         echo"<pre>";
         var_dump($openMeteoObject);
         echo"</pre>";
-        $hourlyTemp = $this->openMeteoObject->hourly->temperature_2m;
-        $hourlyHumidity = $this->openMeteoObject->data->hourly->relativehumidity_2m;
-        $hourlyWeathercode = $this->openMeteoObject->data->hourly->weathercode;
-        $dailyMinTemp = $this->openMeteoObject->data->daily->temperature_2m_min;
-        $dailyMaxTemp = $this->openMeteoObject->data->daily->temperature_2m_max;
-        $dailyWeathercode = $this->openMeteoObject->data->daily->weathercode;
-        $dailyWindSpeed = $this->openMeteoObject->data->daily->windspeed_10m_max;
+        $this->hourlyTemp = $openMeteoObject->hourly->temperature_2m;
+        $this->hourlyHumidity = $openMeteoObject->hourly->relativehumidity_2m;
+        $this->hourlyWeathercode = $openMeteoObject->hourly->weathercode;
+        $this->dailyMinTemp = $openMeteoObject->daily->temperature_2m_min;
+        $this->dailyMaxTemp = $openMeteoObject->daily->temperature_2m_max;
+        $this->dailyWeathercode = $openMeteoObject->daily->weathercode;
+        $this->dailyWindSpeed = $openMeteoObject->daily->windspeed_10m_max;
         
     }
     function getHourlyTemp(){
